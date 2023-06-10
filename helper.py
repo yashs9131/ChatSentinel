330d67879de79446b884163cbb43740ba4c161dc
yashs9131 <yashs9131@gmail.com>
2023-06-10 22:29:56 +0530

    initial commit

---
+++ helper.py
@@ -0,0 +1,215 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from urlextract import URLExtract
+from wordcloud import WordCloud
+from collections import Counter
+from PIL import Image
+import pandas as pd
+import numpy as np
+import string
+import emoji
+
+url_extract = URLExtract()
+sia = SentimentIntensityAnalyzer()
+
+
+def change_user(selected_user, df):
+    if selected_user != "General":
+        df = df[df["user"] == selected_user]
+    return df
+
+
+def fetch_stats(selected_user, df):
+    df = change_user(selected_user, df)
+
+    # Dropping voice and video call notifications
+    df = df[(df["message"] != "Missed voice call\n") & (df["message"] != "Missed video call\n")].reset_index(drop=True)
+
+    # 1. Fetch total number of messages
+    total_messages = df.shape[0]
+
+    # 2. Fetch total number of media messages
+    total_media = df[df["message"] == "<Media omitted>\n"].shape[0]
+    df_text = df[df["message"] != "<Media omitted>\n"].reset_index(drop=True)
+
+    # 3. Fetch total number of links shared
+    links = 0
+    messages = []
+    for message in df_text["message"]:
+        if url_extract.has_urls(message):
+            urls = url_extract.find_urls(message)
+            links += 1
+            for url in urls:
+                # Replaced by a stopword, so it does not come up in the wordcloud later
+                message.replace(url, "uss")
+        messages.append(message)
+
+    df_text["message"] = messages
+    df_text = df_text[df_text["message"] != "null\n"].reset_index(drop=True)
+
+    # 4. Fetch total number of words
+    words = 0
+    tags_len = 0
+    con_len = 0
+    for message in df_text['message']:
+        if message.find("(file attached)\n") != -1:
+            con_len += 1
+        for word in message.lower().split():
+            if word[0] == "@" and len(word) != 1:
+                tags_len += 1
+            else:
+                words += 1
+
+    return total_messages, words, total_media, links, tags_len, df_text, con_len
+
+
+def message_frequency(df):
+    ratio = df["user"].value_counts() / df.shape[0]
+    return df["user"].value_counts(), ratio
+
+
+def create_word_cloud(selected_user, df_text):
+    df_text = change_user(selected_user, df_text)
+
+    indices = []
+    new_messages = []
+
+    table = str.maketrans("", "", string.punctuation)
+    with open("./stop_words.txt", "r", encoding="utf-8") as file:
+        stop_words = file.read().splitlines()
+
+    for i in range(df_text.shape[0]):
+        words = []
+        for word in df_text["message"][i].lower().split():
+            if word[0] == "@" and len(word) != 1:
+                if i not in indices:
+                    indices.append(i)
+            else:
+                # Here, I replaced a special type of apostrophe I found only in case of apple users
+                words.append(word.replace("’", ""))
+        stripped = [w.translate(table) for w in words]  # Stripping of punctuations
+        cleaned = [word for word in stripped if word not in stop_words]  # Cleaning of the stop words
+        denumbered = [word for word in cleaned if not word.isdigit()]
+        new_messages.append(" ".join(denumbered))
+
+    df_tags = df_text.iloc[indices, :].reset_index(drop=True)
+    df_text["message"] = new_messages
+    df_text = df_text[df_text["message"] != ""]
+
+    wc_mask = np.array(Image.open("Images/chat-icon-vector-illustration (2000).jpg"))
+
+    wc = WordCloud(width=1368, height=1024, mask=wc_mask, min_font_size=12, background_color="white")
+    df_wc = wc.generate(df_text['message'].str.cat(sep=" "))
+
+    return df_wc, df_text, df_tags, True
+
+
+def most_common_words(selected_user, df_text):
+    df_text = change_user(selected_user, df_text)
+
+    words = []
+    for message in df_text["message"]:
+        for word in message.split():
+            words.append(word)
+
+    return pd.DataFrame(Counter(words).most_common(20), columns=["word", "frequency"])
+
+
+def emoji_helper(selected_user, df_emoji):
+    df_emoji = change_user(selected_user, df_emoji)
+
+    emojis = []
+    for message in df_emoji["message"]:
+        emojis_dict = emoji.emoji_list(message)
+        for i in range(len(emojis_dict)):
+            emojis.append(emojis_dict[i]["emoji"])
+
+    my_df = pd.DataFrame(Counter(emojis).most_common(10), columns=["emoji", "count"])
+
+    return my_df
+
+
+def monthly_timeline(selected_user, df):
+    df = change_user(selected_user, df)
+
+    m_timeline = df.groupby(["year", "month_num", "month"]).count()["message"].reset_index()
+
+    time = []
+    for i in range(m_timeline.shape[0]):
+        time.append(m_timeline["month"][i][:3] + "-" + str(m_timeline["year"][i])[2:])
+
+    m_timeline["time"] = time
+
+    return m_timeline
+
+
+def daily_timeline(selected_user, df):
+    df = change_user(selected_user, df)
+    d_timeline = df.groupby("only_date").count()["message"].reset_index()
+
+    return d_timeline
+
+
+def activity_per_day(selected_user, df):
+    df = change_user(selected_user, df)
+
+    day_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    df_day = pd.DataFrame(df["day_name"].value_counts(), columns=["day_name"])
+    df_day["day_num"] = [(day_list.index(day) + 1) for day in df_day.index]
+    df_day.sort_values(by="day_num", inplace=True)
+    df_day.index = [x[:3] for x in df_day.index]
+
+    return df_day
+
+
+def activity_per_month(selected_user, df):
+    df = change_user(selected_user, df)
+
+    df_month = pd.DataFrame({"month_name": df["month"].value_counts().values,
+                             "month_num": [(list(df.sort_values(by="month_num")["month"].unique()).index(mon) + 1)
+                                           for mon in df["month"].value_counts().index]},
+                            index=df["month"].value_counts().index)
+
+    df_month.sort_values(by="month_num", inplace=True)
+    df_month.index = [x[:3] for x in df_month.index]
+
+    return df_month
+
+
+def activity_heatmap(selected_user, df):
+    df = change_user(selected_user, df)
+    period = []
+    for hour in df[["day_name", "hour"]]["hour"]:
+        if hour == 23:
+            period.append(str(hour) + "-" + "00")
+        elif hour < 9:
+            period.append("0" + str(hour) + "-0" + str(hour + 1))
+        elif hour == 9:
+            period.append("0" + str(hour) + "-" + str(hour + 1))
+        elif hour > 9:
+            period.append(str(hour) + "-" + str(hour + 1))
+
+    df["period"] = period
+
+    activity = df.pivot_table(index="day_name", columns="period", values="message", aggfunc="count").fillna(0)
+
+    return activity
+
+
+def message_sa(message):
+    compound_sentiment = sia.polarity_scores(message)["compound"]
+    if compound_sentiment > 0:
+        return "Positive"
+    elif compound_sentiment < 0:
+        return "Negative"
+    else:
+        return "Neutral"
+
+
+def sentiment_analysis(selected_user, df):
+    df = change_user(selected_user, df)
+    df_sa = df[["user", "message"]]
+    df_sa["sentiments"] = df_sa["message"].apply(message_sa)
+    df_sa = df_sa[df_sa["sentiments"] != "Neutral"]
+    df_sa.sort_values(by=["sentiments"], ascending=True, inplace=True)
+    return df_sa
---
+++ preprocessor.py
@@ -0,0 +1,99 @@
+import re
+import emoji
+import pandas as pd
+
+
+def preprocess(data, time_format):
+    """It preprocesses the given input file to form a DataFrame of meaningful features"""
+
+    pattern = ""
+    if time_format == "12 hour clock":
+        pattern = "\d{1,2}\/\d{2,4}\/\d{2,4},\s\d{1,2}:\d{1,2}\s\w{2}\s-\s"
+    elif time_format == "24 hour clock":
+        pattern = "\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s"
+    user_messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({"user_message": user_messages, "message_date": dates})
+
+    if time_format == "24 hour clock":
+        try:
+            df["message_date"] = pd.to_datetime(df["message_date"], format='%d/%m/%y, %H:%M - ')
+        except ValueError:
+            try:
+                df["message_date"] = pd.to_datetime(df["message_date"], format='%d/%m/%Y, %H:%M - ')
+            except ValueError:
+                try:
+                    df["message_date"] = pd.to_datetime(df["message_date"], format='%m/%d/%Y, %H:%M - ')
+                except ValueError:
+                    df["message_date"] = pd.to_datetime(df["message_date"], format='%m/%d/%y, %H:%M - ')
+    elif time_format == "12 hour clock":
+        try:
+            df["message_date"] = pd.to_datetime(df["message_date"], format='%d/%m/%y, %I:%M %p - ')
+        except ValueError:
+            try:
+                df["message_date"] = pd.to_datetime(df["message_date"], format='%d/%m/%Y, %I:%M %p - ')
+            except ValueError:
+                try:
+                    df["message_date"] = pd.to_datetime(df["message_date"], format='%m/%d/%Y, %I:%M %p - ')
+                except ValueError:
+                    df["message_date"] = pd.to_datetime(df["message_date"], format='%m/%d/%y, %I:%M %p - ')
+
+    df.rename(columns={"message_date": "date"}, inplace=True)
+
+    # Separating Users and Text Messages
+    users = []
+    messages = []
+    for message in df["user_message"]:
+        entry = re.split("([\w\W]+?):\s", message)
+        if entry[1:]:  # User Name
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append("Notification")
+            messages.append(entry[0])
+
+    df["user"] = users
+    df["message"] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+    # Dropping notifications
+    df = df[df["user"] != "Notification"].reset_index(drop=True)
+
+    # Dropping and counting deleted messages by different users
+    df_deleted = df[(df["message"] == "This message was deleted\n") | (df["message"] == "You deleted this message\n")]\
+        .reset_index(drop=True)
+    df = df[(df["message"] != "This message was deleted\n") & (df["message"] != "You deleted this message\n")]\
+        .reset_index(drop=True)
+
+    df["year"] = df["date"].dt.year
+    df["day"] = df["date"].dt.day
+    df["hour"] = df["date"].dt.hour
+    df["minute"] = df["date"].dt.minute
+    df["month_num"] = df["date"].dt.month
+    df["only_date"] = df["date"].dt.date
+    df["day_name"] = df["date"].dt.day_name()
+
+    months = []
+    month_list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                  "November", "December"]
+
+    for date in df["date"]:
+        months.append(month_list[int(date.month) - 1])
+
+    df["month"] = months
+
+    # Creating a separate Dataframe for emojis and removing from the current dataframe
+    df["emojis"] = df["message"].apply(emoji.emoji_count)
+
+    # Extracting rows with emojis in message
+    df_emoji = df[df['emojis'] != 0].reset_index(drop=True)
+
+    # Dropping extra feature
+    df.drop(["emojis"], axis=1, inplace=True)
+
+    # Removing emojis from current dataframe
+    df["message"] = df["message"].apply(emoji.replace_emoji)
+
+    # Till we don't have a use for the deleted messages dataset, we will only return its length.
+    return df, df_deleted, df_emoji
